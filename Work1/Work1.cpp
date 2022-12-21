83081ccdbb9ca932fe0117a2909809d275f60c9b
Anna <annkotsan@gmail.com>
2022-12-21 22:33:53 +0300

    Внесение изменений

--- Work1/Work1.cpp
+++ Work1/Work1.cpp
@@ -11,34 +11,54 @@ int main()
     int size2;
 
     std::ifstream fin("C:\\Users\\Ann\\Desktop\\HomeWorks\\Work1\\x64\\Debug\\in.txt");
+    // Массивы сразу пишем в правильной очередности чисел, со сдвигом вправо и со сдвигом влево
     fin >> size1;
     // Первый массив
     int* arr1 = new int[size1];
-    for (int i = 0; i < size1; i++) 
-        fin >> arr1[i];
-
+    for (int i = 0; i < size1; i++)
+    {        
+        if (i == 0)
+        {
+            fin >> arr1[size1-1];
+        }
+        else
+        {
+            fin >> arr1[i-1];
+        }
+    }
         fin >> size2;
         // Второй массив
         int* arr2 = new int[size2];
         for (int j= 0; j < size2; j++) 
-            fin >> arr2[j];
+        {
+            if (j == size2 - 1)
+            {
+                fin >> arr2[0];
+            }
+            else
+            {
+                fin >> arr2[j + 1];
+            }
+        }
     
     fin.close();
 
-    // Запись в файл
-    std::ofstream fout("C:\\Users\\Ann\\Desktop\\HomeWorks\\Work1\\x64\\Debug\\out.txt");
-    fout << size1 << std::endl;
-    for (int i = size1 - 1; i >= 0; i--)
-    {
-        fout << arr1[i] << " ";
-    }
-    fout << std::endl;
 
+    // Запись в файл (массивы выводим наоборот)
+    std::ofstream fout("C:\\Users\\Ann\\Desktop\\HomeWorks\\Work1\\x64\\Debug\\out.txt");
     fout << size2 << std::endl;
-    for (int i = size2 - 1; i >= 0; i--)
+    for (int i = 0; i < size2; i++)
     {
         fout << arr2[i] << " ";
     }
+    fout << std::endl;
+
+    fout << size1 << std::endl;
+    for (int i = 0; i < size1; i++)
+    {
+        fout << arr1[i] << " ";
+    }
+
     fout << std::endl;
     fout.close();
 
---
+++ Work2_1/Work2_1.cpp
@@ -0,0 +1,75 @@
+﻿
+#include <iostream>
+
+ enum Month
+{
+	 Jan=1,
+	 Feb,
+	 Mar,
+	 Apr,
+	 May,
+	 Jun,
+	 Jul,
+	 Aug,
+	 Sep,
+	 Oct,
+	 Nov,
+	 Dec
+};
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+	int val;
+	Month number;
+	while (val != 0)
+	{
+		std::cout << "Введите номер месяца: ";
+		std::cin >> val;
+		number = static_cast<Month>(val);
+		std::cout << std::endl;
+		switch (number)
+		{
+		case Jan:
+			std::cout << "Январь";
+			break;
+		case Feb:
+			std::cout << "Февраль";
+			break;
+		case Mar:
+			std::cout << "Март";
+			break;
+		case Apr:
+			std::cout << "Апрель";
+			break;
+		case May:
+			std::cout << "Май";
+			break;
+		case Jun:
+			std::cout << "Июнь";
+			break;
+		case Jul:
+			std::cout << "Июль";
+			break;
+		case Aug:
+			std::cout << "Август";
+			break;
+		case Sep:
+			std::cout << "Сентябрь";
+			break;
+		case Oct:
+			std::cout << "Октябрь";
+			break;
+		case Nov:
+			std::cout << "Ноябрь";
+			break;
+		case Dec:
+			std::cout << "Декабрь";
+			break;
+		default:
+			std::cout << "Неправильный номер!";
+			break;
+		}
+	}
+	std::cout << "До свидания!";
+}
