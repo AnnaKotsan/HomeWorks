e5710299c4a3a9c4b6e127d53826466b4cdbe666
Anna <annkotsan@gmail.com>
2023-01-22 22:40:55 +0300

    FirstVersion

--- Work_5_1/Work_5_1.cpp
+++ Work_5_1/Work_5_1.cpp
@@ -1,20 +1,63 @@
-﻿// Work_5_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <Windows.h> // для SetConsoleCP() и SetConsoleOutputCP()
 #include <iostream>
+
+//В этом задании вы построите первую простую иерархию классов для заданной предметной области.
+//
+//Предметная область выглядит следующим образом : есть сущность фигура.Фигуры бывают двух типов : треугольник и четырёхугольник.У фигуры есть количество сторон.Для неизвестной фигуры будем считать это количество равным нулю.У треугольника три стороны, у четырёхугольника четыре стороны.У любой фигуры мы хотим узнавать количество её сторон, но мы не должны быть способны изменить количество сторон для заданной фигуры извне.
+//
+//Задача : спроектировать и реализовать классы, описывающие предметную область.Продемонстрируйте их работу : создайте по одному экземпляру каждого класса и выведите информацию о количестве его сторон на консоль.
+
+
+class Figure
+{
+protected:
+    int CountSides;
+    std::string NameFigure;
+public:
+    Figure(std::string namefigure)
+    {
+        CountSides = 0;
+        NameFigure = namefigure;
+    }
+
+    std::string GetName()
+    {
+        return NameFigure;
+    }
+    
+    int GetCountSides()
+    {
+        return CountSides;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    Triangle(int countsides, std::string namefigure) : Figure(namefigure)
+    {        
+            CountSides = countsides;
+    }
+};
+
+class Quadrangle : public Figure
+{
+public:
+    Quadrangle(int countsides, std::string namefigure) : Figure(namefigure)
+    {
+        CountSides = countsides;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Количество сторон: \n";
+    Figure figure = Figure("Фигура");
+    Triangle triangle = Triangle(3, "Треугольник");
+    Quadrangle quadrangle = Quadrangle(4, "Четырехугольник");
+    std::cout << figure.GetName() << ": " << figure.GetCountSides() << std::endl;
+    std::cout << triangle.GetName() << ": " << triangle.GetCountSides() << std::endl;
+    std::cout << quadrangle.GetName() << ": " << quadrangle.GetCountSides() << std::endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
